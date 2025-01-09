249524eda05dbbc87d4f1c12f178470e14d14b87
Sammi Rosser <samanthajaderosser@gmail.com>
2025-01-09 11:06:43 +0000

    Add simultaneous play button code for streamlit
    Requires st_javascript as optional dependency

--- vidigi/utils.py
+++ vidigi/utils.py
@@ -326,3 +326,72 @@ def event_log_from_ciw_recs(ciw_recs_obj, node_name_list):
                 )
 
     return pd.DataFrame(event_logs)
+
+def streamlit_play_all():
+    try:
+        from streamlit_javascript import st_javascript
+
+        st_javascript("""new Promise((resolve, reject) => {
+    console.log('You pressed the play button');
+
+    const parentDocument = window.parent.document;
+
+    // Define playButtons at the beginning
+    const playButtons = parentDocument.querySelectorAll('g.updatemenu-button text');
+
+    let buttonFound = false;
+
+    // Create an array to hold the click events to dispatch later
+    let clickEvents = [];
+
+    // Loop through all found play buttons
+    playButtons.forEach(button => {
+        if (button.textContent.trim() === '▶') {
+        console.log("Queueing click on button");
+        const clickEvent = new MouseEvent('click', {
+            view: window,
+            bubbles: true,
+            cancelable: true
+        });
+
+        // Store the click event in the array
+        clickEvents.push(button.parentElement);
+        buttonFound = true;
+        }
+    });
+
+    // If at least one button is found, dispatch all events
+    if (buttonFound) {
+        console.log('Dispatching click events');
+        clickEvents.forEach(element => {
+        element.dispatchEvent(new MouseEvent('click', {
+            view: window,
+            bubbles: true,
+            cancelable: true
+        }));
+        });
+
+        resolve('All buttons clicked successfully');
+    } else {
+        reject('No play buttons found');
+    }
+    })
+    .then((message) => {
+    console.log(message);
+    return 'Play clicks completed';
+    })
+    .catch((error) => {
+    console.log(error);
+    return 'Operation failed';
+    })
+    .then((finalMessage) => {
+    console.log(finalMessage);
+    });
+
+    """)
+
+    except ImportError:
+        raise ImportError(
+            "This function requires the dependency 'st_javascript', but this is not installed with vidigi by default. "
+            "Install it with: pip install vidigi[helper]"
+        )
