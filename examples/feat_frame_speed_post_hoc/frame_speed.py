c7403dc28b240ac285a151b888dc476970886baa
Sammi Rosser <samanthajaderosser@gmail.com>
2026-05-14 15:36:00 +0100

    fix(docs): switch to relative imports for all examples

--- examples/feat_frame_speed_post_hoc/frame_speed.py
+++ examples/feat_frame_speed_post_hoc/frame_speed.py
@@ -5,7 +5,7 @@ from pathlib import Path
 ROOT = Path(__file__).resolve().parents[2]
 sys.path.append(str(ROOT))
 
-from examples.example_2_branching_multistep.ex_2_model_classes import g, Trial
+from ex_2_model_classes import g, Trial
 from vidigi.utils import EventPosition, create_event_position_df
 from vidigi.animation import animate_activity_log
 
